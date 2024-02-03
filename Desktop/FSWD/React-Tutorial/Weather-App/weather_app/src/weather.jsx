cb1cf5771e84130f09c258d53f52243b091c7f99
Metamorf999 <meena999csc@gmail.com>
2024-02-03 17:39:12 +0000

    Added new files and modified existing ones

---
+++ Desktop/FSWD/React-Tutorial/Weather-App/weather_app/src/weather.jsx
@@ -0,0 +1,11 @@
+function Weather()
+{
+    return(
+        <div className="bg-black p-20">
+            <div className="bg-green p-10 rounded-md">
+
+            </div>
+
+        </div>
+    )
+}
